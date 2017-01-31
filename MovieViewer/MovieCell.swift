395e7cc5cb3af59bc0ac8b428b13a1d7ffa670c7
Lin Zhou <zhoulin@lins-mbp.attlocal.net>
2017-01-30 22:33:36 -0800

    Monday work

---
+++ MovieViewer/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  MovieViewer
+//
+//  Created by Lin Zhou on 1/30/17.
+//  Copyright © 2017 Lin Zhou. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
